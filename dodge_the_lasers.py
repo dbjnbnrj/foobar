4d3dd021784d38edf0663375bf5c8750f533d842
Debjani Banerjee <dnbanerjee@ebay.com>
2016-11-04 08:00:42 -0700

    Modified dodger

--- dodge_the_lasers.py
+++ dodge_the_lasers.py
@@ -17,7 +17,11 @@ floor(5*sqrt(2))
 = 1+2+4+5+7 = 19
 so the function would return "19".
 
-str_n will be a positive integer between 1 and 10^100, inclusive. Since n can be very large (up to 101 digits!), using just sqrt(2) and a loop won't work. Sometimes, it's easier to take a step back and concentrate not on what you have in front of you, but on what you don't.
+str_n will be a positive integer between 1 and 10^100, inclusive.
+Since n can be very large (up to 101 digits!),
+using just sqrt(2) and a loop won't work.
+Sometimes, it's easier to take a step back and concentrate
+not on what you have in front of you, but on what you don't.
 
 Languages
 =========
@@ -40,3 +44,77 @@ Output:
 
 Use verify [file] to test your solution and see how it does. When you are finished editing your code, use submit [file] to submit your answer. If your solution passes the test cases, it will be removed from your home folder.
 '''
+
+
+import math as m
+import time as t
+import math as m
+import time as t
+def answer(str_n):
+    if len(str_n) > 18:
+        return 0
+    N = int(str_n)
+    LW = lambda x : m.floor(x*m.sqrt(2))
+    UW = lambda x : m.floor(x*(2+m.sqrt(2)))
+
+    def getSum(num):
+        return num*(num+1)/2
+
+    def getMid(N):
+        #print m.log(N, 10)
+        start, end= 0, N
+        mid = m.floor((end + start)/2)
+        num = LW(N)
+        steps = 0
+        while True:
+            if start > end:
+                break
+            steps +=1
+            mid = m.floor((end + start)/2)
+            if UW(mid) < num < UW(mid+1):
+                print 'steps; ',steps, N
+                return int(mid)
+            elif UW(mid) < num:
+                start = mid + 1
+            else:
+                end = mid - 1
+
+        return int(mid)
+
+    def getResult(x, y):
+        return getSum(LW(x)) - sum([UW(i) for i in range(y+1)])
+
+    mid = getMid(N)
+    A = [N, mid]
+    #print N, mid
+    LIM = pow(10, 6)
+    if mid < LIM:
+        result = getSum(LW(N)) - mid*(mid+1) - sum([LW(i) for i in range(mid+1)])
+    else:
+        mid = getMid(N)
+        A = [N, mid]
+        while mid >= LIM:
+            mid = getMid(mid)
+            A+= [mid]
+
+        mid1, num1 = A.pop(), A.pop()
+        #print mid1, num1
+        temp1 = getSum(LW(num1)) - mid1*(mid1+1) - sum([LW(i) for i in range(mid1 +1)])
+        A+=[num1]
+        while len(A) > 1:
+            #print A
+            mid2, num2 = A.pop(), A.pop()
+            temp2 = getSum(LW(num2)) - mid2*(mid2+1) - temp1
+            temp1 = temp2
+            A+=[num2]
+        result = temp1
+    s = '%d' % result
+    return s.rstrip('.0')
+    #return temp1
+
+
+
+print answer("5")
+print answer("77")
+print answer("775")
+print answer(`pow(10, 6)`)
